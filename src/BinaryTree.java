fe3cb8e4e72fa9390dc4535e63874be9ef204c31
nilsmuralles <murallesnils11@gmail.com>
2024-03-19 21:04:25 -0500

    Búsquda implementada

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,15 +1,34 @@
 package src;
 
+/**
+ * @author Nils Muralles
+ * @since 18/03/24
+ * @description Clase árbol binario que implementa la inserción y la búsqueda
+ */
+
 public class BinaryTree<K extends Comparable<K>, V> {
     private Node<Association<K, V>> root;
  
+    /**
+     * Constructor del Binary Tree
+     */
     public BinaryTree() {}
 
+    /**
+     * Método que inserta un nuevo objeto en base a su clave y su valor (Para usuario)
+     * @param key Clave
+     * @param value Valor 
+     */
     public void insert(K key, V value) {
        insert(root, new Association<>(key, value));
     }
 
-    public void insert(Node<Association<K,V>> current, Association<K, V> association) {
+    /**
+     * Método privado que inserta una asociación a un nodo (interno)
+     * @param current Nodo actual
+     * @param association Asociación que se va a insertar
+     */
+    private void insert(Node<Association<K,V>> current, Association<K, V> association) {
         if (current == null) {
             root = new Node<Association<K,V>>(association);
             return;
@@ -33,5 +52,42 @@ public class BinaryTree<K extends Comparable<K>, V> {
                 current.setLeft(new Node<Association<K,V>>(association));
             }
         }
+    }
+
+    /**
+     * Método de búsqueda en base a una clave (para usuario)
+     * @param key Clave que se busca
+     * @return Valor de la clave
+     */
+    public V search(K key) {
+        return search(root, key);
+    }
+
+    /**
+     * Método privado que busca una clave en el nodo actual
+     * @param current Nodo actual
+     * @param key Clave que se busca
+     * @return Valor de la clave
+     */
+    private V search(Node<Association<K,V>> current, K key) {
+        Association<K,V> temp = new Association<K,V>(key, null);
+        
+        if (current.getValue().getKey().equals(key)) {
+            return current.getValue().getValue();
+        }
+
+        if (temp.compareTo(current.getValue()) == 1) {
+            if (current.getRight() != null) {
+                return search(current.getRight(), key);
+            }
+        }
+
+        if (temp.compareTo(current.getValue()) == -1) {
+            if (current.getLeft() != null) {
+                return search(current.getLeft(), key);
+            }
+        }
+
+        return null;
     }
 }
--- test/BinaryTreeTest.java
+++ test/BinaryTreeTest.java
@@ -1,13 +1,21 @@
 package test;
 
+/**
+ * @author Nils Muralles Morales
+ * @since 18/03/24
+ * Tests para el árbol binario
+ */
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-
 import org.junit.Test;
 import src.BinaryTree;
 
 public class BinaryTreeTest {
     
+    /**
+     * Test 1: Insertar valores
+     */
     @Test
     public void testInsert() {
         BinaryTree<String, String> myTree = new BinaryTree<String, String>();
@@ -15,6 +23,9 @@ public class BinaryTreeTest {
         myTree.insert("Apple", "Manzana");
     }
 
+    /**
+     * Test 2: Insertar valores duplicados devuelve una excepción
+     */
     @Test
     public void testInsertDuplicatedValue() {
         BinaryTree<String, String> myTree = new BinaryTree<String, String>();
@@ -24,6 +35,18 @@ public class BinaryTreeTest {
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> myTree.insert("Apple", "Manzana"));
         assertEquals("Duplicated value: Cannot repeat values in Binary Tree", exception.getMessage());
+    }
+
+    /**
+     * Test 3: La búsqueda funciona correctamente
+     */
+    @Test
+    public void testLookingForAWord() {
+        BinaryTree<String, String> myTree = new BinaryTree<String, String>();
+        myTree.insert("Tree", "Arbol");
+        myTree.insert("Apple", "Manzana");
+        myTree.insert("Dog", "Perro");
+        assertEquals("Perro", myTree.search("Dog"));
     }
  
 }
