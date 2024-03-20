2980ebb2994476229d55847389acf2186c8bab88
nilsmuralles <murallesnils11@gmail.com>
2024-03-19 20:20:54 -0500

    Optimizando inserción, añadiendo excepción por duplicados

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,32 +1,36 @@
 package src;
 
-public class BinaryTree<T extends Comparable<T>> {
-    private Node<T> root;
+public class BinaryTree<K extends Comparable<K>, V> {
+    private Node<Association<K, V>> root;
  
     public BinaryTree() {}
 
-    public void insert(T value) {
-       insert(root, value);
+    public void insert(K key, V value) {
+       insert(root, new Association<>(key, value));
     }
 
-    public void insert(Node<T> current, T value) {
+    public void insert(Node<Association<K,V>> current, Association<K, V> association) {
         if (current == null) {
-            root = new Node<T>(value);
+            root = new Node<Association<K,V>>(association);
             return;
         }
 
-        if (value.compareTo(current.getValue()) == 1) {
+        if (current.getValue().getKey().equals(association.getKey())) {
+            throw new IllegalArgumentException("Duplicated value: Cannot repeat values in Binary Tree");
+        }
+
+        if (association.compareTo(current.getValue()) == 1) {
             if (current.getRight() != null) {
-                insert(current.getRight(), value);
+                insert(current.getRight(), association);
             } else {
-                current.setRight(new Node<T>(value));
+                current.setRight(new Node<Association<K,V>>(association));
             }
 
-        } else if (value.compareTo(current.getValue()) == -1) {
+        } else if (association.compareTo(current.getValue()) == -1) {
             if (current.getLeft() != null) {
-                insert(current.getLeft(), value);
+                insert(current.getLeft(), association);
             } else {
-                current.setLeft(new Node<T>(value));
+                current.setLeft(new Node<Association<K,V>>(association));
             }
         }
     }
--- test/BinaryTreeTest.java
+++ test/BinaryTreeTest.java
@@ -1,16 +1,29 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import src.Association;
 import src.BinaryTree;
 
 public class BinaryTreeTest {
     
     @Test
     public void testInsert() {
-        BinaryTree<Association<String, String>> myTree = new BinaryTree<Association<String, String>>();
-        myTree.insert(new Association<String,String>("Tree", "Árbol"));
-        myTree.insert(new Association<String,String>("Apple", "Manzana"));
+        BinaryTree<String, String> myTree = new BinaryTree<String, String>();
+        myTree.insert("Tree", "Árbol");
+        myTree.insert("Apple", "Manzana");
+    }
+
+    @Test
+    public void testInsertDuplicatedValue() {
+        BinaryTree<String, String> myTree = new BinaryTree<String, String>();
+        myTree.insert("Tree", "Árbol");
+        myTree.insert("Pineapple", "Piña");
+        myTree.insert("Apple", "Manzana");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> myTree.insert("Apple", "Manzana"));
+        assertEquals("Duplicated value: Cannot repeat values in Binary Tree", exception.getMessage());
     }
  
 }
